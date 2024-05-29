6e76bd5e703d536ee5a6ad3843d0a788e3ef49eb
The Coder <thecoder38@gmail.com>
2024-05-29 02:13:49 +0100

    initial structure

--- project/lib/src/shared/services/mongoose/configs/index_config.ts
+++ project/lib/src/shared/services/mongoose/configs/index_config.ts
@@ -4,7 +4,7 @@ class IndexConfig {
    * createIndex
    */
   public static async createIndex() {
-    // await UserModel.createIndexes(); => to create the index of user model
+    // await UserModel.createIndexes(); => to create the index of user model , so available it
   }
 }
 
